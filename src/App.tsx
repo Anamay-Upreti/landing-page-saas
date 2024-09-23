d61a26a6c6aedb204aa888bfba2f528e80ff2005
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-09-23 23:01:05 +0530

    added-assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-
-const App = () => {
+export default function App() {
   return (
-    <div>App</div>
+    <div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
   )
 }
-
-export default App
