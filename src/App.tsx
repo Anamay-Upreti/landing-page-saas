50b277493c6439aec3a22a8dd0cbd960fd32166b
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-09-23 23:05:13 +0530

    started-header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
+import Header from "./sections/Header.jsx"
 export default function App() {
   return (
-    <div>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
+    <main className="overflow-hidden">
+      <Header/>
+    </main>
   )
 }
