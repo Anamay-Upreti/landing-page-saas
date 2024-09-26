2332561adb7e119bd52143724cda8d37848a0ec2
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-09-26 23:08:00 +0530

    started-hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Header from "./sections/Header.jsx"
+import Hero from "./sections/Hero.jsx"
 export default function App() {
   return (
     <main className="overflow-hidden">
       <Header/>
+      <Hero/>
     </main>
   )
 }
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+   <section className='relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32 '>
+     
+   </section>
+  )
+}
+
+export default Hero
