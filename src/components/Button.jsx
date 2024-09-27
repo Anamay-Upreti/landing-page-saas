3c4177deb61d05757a93ff596479c9a7a7b1c849
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-09-27 22:15:54 +0530

    added-base-button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,28 @@
 import clsx from "clsx"
 
-const Button = ({ icon, children}) => {
-  return (
-    <button className={clsx('relative p-0.5 g5 rounded-2xl shadow-500 group')}></button>
+const Button = ({ icon, children, href, containerClassName }) => {
+    const Inner = () => (
+        <>
+        <span>
+            <span>
+                Marker
+            </span>
+        </span>
+        </>
+    )
+  return href ? (
+    <a className={clsx('relative p-0.5 g5 rounded-2xl shadow-500 group', containerClassName, onClick)}
+    href={href}   
+    >
+     <Inner/>
+    </a>
+  ) : (
+    <button className={clsx('relative p-0.5 g5 rounded-2xl shadow-500 group', containerClassName,
+    )}
+    onClick= {onClick}
+    >
+        <Inner/>
+    </button>
   )
 }
 
