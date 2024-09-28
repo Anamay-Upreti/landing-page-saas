74ce1256815821ffd44028bb6adf4975913028e6
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-09-28 23:39:30 +0530

    added-button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,37 +1,47 @@
-import clsx from "clsx"
+import React from 'react';
+import clsx from 'clsx';
+import { Marker } from './Marker.jsx';  // Ensure this file exists
 
-const Button = ({ icon, children, href, containerClassName }) => {
-    const Inner = () => (
-        <>
-        <span>
-            <span>
-                Marker
-            </span>
-            {
-              icon && (
-                <img src={icon} alt="circle" className="size-10 mr-5 object-contain z-10"/>
-              )
-            }
-            <span>
-              {children}
-            </span>
+const Button = ({ icon, children, href, containerClassName, onClick, markerFill }) => {
+  const Inner = () => (
+    <>
+      <span className="relative flex items-center min-h-[60px] px-4 bg-gray-700 rounded-2xl inner-before group-hover:before:opacity-100 overflow-hidden">
+        <span className="absolute -left-[1px]">
+          <Marker markerFill={markerFill} />
         </span>
-        </>
-    )
+
+        {icon && (
+          <img
+            src={icon}
+            alt="circle"
+            className="w-10 h-10 mr-5 object-contain z-10"
+          />
+        )}
+
+        <span className="relative z-2 font-bold text-white uppercase">
+          {children}
+        </span>
+      </span>
+
+      <span className="absolute inset-0 rounded-2xl bg-gradient-to-r from-purple-400 to-blue-500 opacity-75" />
+    </>
+  );
+
   return href ? (
-    <a className={clsx('relative p-0.5 g5 rounded-2xl shadow-500 group', containerClassName, onClick)}
-    href={href}   
+    <a
+      className={clsx("relative p-0.5 rounded-2xl shadow-lg group", containerClassName)}
+      href={href}
     >
-     <Inner/>
+      <Inner />
     </a>
   ) : (
-    <button className={clsx('relative p-0.5 g5 rounded-2xl shadow-500 group', containerClassName,
-    )}
-    onClick= {onClick}
+    <button
+      className={clsx("relative p-0.5 rounded-2xl shadow-lg group", containerClassName)}
+      onClick={onClick}
     >
-        <Inner/>
+      <Inner />
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
