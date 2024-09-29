0232b86b37e96830cf45a82d87693f1514b21396
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-09-29 17:05:46 +0530

    issuefixed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import clsx from 'clsx';
-import { Marker } from './Marker.jsx';  // Ensure this file exists
+import clsx from "clsx";
+import { Marker } from "./Marker.jsx";
 
-const Button = ({ icon, children, href, containerClassName, onClick, markerFill }) => {
+const Button = ({
+  icon,
+  children,
+  href,
+  containerClassName,
+  onClick,
+  markerFill,
+}) => {
   const Inner = () => (
     <>
-      <span className="relative flex items-center min-h-[60px] px-4 bg-gray-700 rounded-2xl inner-before group-hover:before:opacity-100 overflow-hidden">
+      <span className="relative flex items-center min-h-[60px] px-4 g4 rounded-2xl inner-before group-hover:before:opacity-100 overflow-hidden">
         <span className="absolute -left-[1px]">
           <Marker markerFill={markerFill} />
         </span>
@@ -14,34 +20,38 @@ const Button = ({ icon, children, href, containerClassName, onClick, markerFill 
           <img
             src={icon}
             alt="circle"
-            className="w-10 h-10 mr-5 object-contain z-10"
+            className="size-10 mr-5 object-contain z-10"
           />
         )}
 
-        <span className="relative z-2 font-bold text-white uppercase">
+        <span className="relative z-2 font-poppins base-bold text-p1 uppercase">
           {children}
         </span>
       </span>
 
-      <span className="absolute inset-0 rounded-2xl bg-gradient-to-r from-purple-400 to-blue-500 opacity-75" />
+      <span className="glow-before glow-after" />
     </>
   );
-
   return href ? (
     <a
-      className={clsx("relative p-0.5 rounded-2xl shadow-lg group", containerClassName)}
+      className={clsx(
+        "relative p-0.5 g5 rounded-2xl shadow-500 group",
+        containerClassName,
+      )}
       href={href}
     >
       <Inner />
     </a>
   ) : (
     <button
-      className={clsx("relative p-0.5 rounded-2xl shadow-lg group", containerClassName)}
+      className={clsx(
+        "relative p-0.5 g5 rounded-2xl shadow-500 group",
+        containerClassName,
+      )}
       onClick={onClick}
     >
       <Inner />
     </button>
   );
 };
-
 export default Button;
